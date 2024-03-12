4ee87e638bdecdaf737e702697a4786c04c86ada
Dima Grossman <dima@grossman.io>
2024-03-12 07:55:05 +0200

    init

---
+++ src/app/echo/client.ts
@@ -0,0 +1,26 @@
+import { Echo } from '@novu/echo';
+
+export const echo = new Echo({
+    /**
+     * This will be needed when you want to sync your work.
+     * Find your API-Key here: https://web.novu.co/settings
+     */
+    apiKey: '375ba111413fe0901a74316bc90efe36',
+    /**
+     * Enable this flag only during local development
+     */
+    devModeBypassAuthentication: process.env.NODE_ENV === 'development'
+});
+
+echo.workflow('hello-world', async ({ step }) => {
+    await step.email('send-email', async (inputs) => {
+        return {
+            subject: "This is an email subject",
+            body: "E-mail body of hello " + inputs.world
+        }
+    }, {
+        inputSchema: {
+            type: 'object', properties: { world: { type: 'string', default: 'World' }}
+        }
+    });
+}, { payloadSchema: { type: 'object', properties: {} }});
