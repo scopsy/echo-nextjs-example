70e2ade6bbe28bcac7df9ebaf30bab9d111dee02
Dima Grossman <dima@grossman.io>
2024-03-12 11:36:13 +0200

    feat: add react email

--- src/app/echo/client.ts
+++ src/app/echo/client.ts
@@ -1,4 +1,5 @@
 import { Echo } from '@novu/echo';
+import { renderReactEmail } from '@/app/echo/react-email';
 
 export const echo = new Echo({
     /**
@@ -16,12 +17,45 @@ echo.workflow('hello-world', async ({ step }) => {
     await step.email('send-email', async (inputs) => {
         return {
             subject: "This is an email subject",
-            body: "E-mail body of hello " + inputs.world
+            body: renderReactEmail(inputs)
         }
     }, {
         inputSchema: {
-            type: 'object', properties: { world: { type: 'string', default: 'World' }}
-        }
+            type: "object",
+            properties: {
+                showJoinButton: { type: "boolean", default: true },
+                username: { type: "string", default: "alanturing" },
+                userImage: {
+                    type: "string",
+                    default:
+                        "https://react-email-demo-bdj5iju9r-resend.vercel.app/static/vercel-user.png",
+                    format: "uri",
+                },
+                invitedByUsername: { type: "string", default: "Alan" },
+                invitedByEmail: {
+                    type: "string",
+                    default: "alan.turing@example.com",
+                    format: "email",
+                },
+                teamName: { type: "string", default: "Team Awesome" },
+                teamImage: {
+                    type: "string",
+                    default:
+                        "https://react-email-demo-bdj5iju9r-resend.vercel.app/static/vercel-team.png",
+                    format: "uri",
+                },
+                inviteLink: {
+                    type: "string",
+                    default: "https://vercel.com/teams/invite/foo",
+                    format: "uri",
+                },
+                inviteFromIp: { type: "string", default: "204.13.186.218" },
+                inviteFromLocation: {
+                    type: "string",
+                    default: "São Paulo, Brazil",
+                },
+            },
+        },
     });
 }, { payloadSchema: { type: 'object', properties: {} }});
 
